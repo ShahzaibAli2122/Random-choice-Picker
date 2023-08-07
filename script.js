7b46bc926e1744c36dac2495089b2de076a33083
shahzaib Ali <98844326+ShahzaibAli2122@users.noreply.github.com>
2023-08-07 22:27:59 +0500

    Complete Code

---
+++ script.js
@@ -0,0 +1,34 @@
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
+
+
+
+textarea.focus()
+
+
+textarea.addEventListener('keyup', (e) =>{
+  createTags(e.target.value)
+})
+
+function createTags (input){
+    const tags = input.split(',').filter(tag => tag.trim()
+    !== "").map(tag => tag.trim())
+    // console.log(tags )
+    tagsEl.innerHTML= ""
+    tags.forEach(tag =>{
+        const tagsEl = document.createElement('span')
+        tagsEl.classList.add('tag')
+        tagsEl.innerText = tag
+        tagsEl.appendChild(tagsEl)
+    } )
+}
+
+
+
+
+
+
+
+// <span class="tag">1</span>
+// <span class="tag highlight">2</span>
+// <span class="tag">3</span>
